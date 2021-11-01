069c10b840b264a562da4e4652f853e25fa352af
StevenMcGown <46934571+StevenMcGown@users.noreply.github.com>
2021-10-31 22:43:23 -0400

    Delete Documents/Personal_Projects directory

--- Documents/Personal_Projects/Angular/PathFinder/express.sh
+++
@@ -1,4 +0,0 @@
-git add .
-read -p "message: " message_var
-git commit -m "$message_var"
-git push
--- Documents/Personal_Projects/Angular/PathFinder/upload.sh
+++
@@ -1,4 +0,0 @@
-ng build
-aws s3 rm s3://pathfind.com --recursive
-aws s3 sync ./dist/ s3://pathfind.com --acl public-read
-#aws cloudfront create-invalidation --distribution-id EO37R0PS8J6V4 --paths "/*"
